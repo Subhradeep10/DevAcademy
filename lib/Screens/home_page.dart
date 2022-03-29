79e3e9a319a7cb5a70c9bc194bb160d5e38de6ba
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-03-29 13:09:51 +0530

    Landing page is started

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -5,23 +5,25 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 0, 44, 109),
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 0, 24, 59),
-        title: const Text(
-          'DevAcademy',
-        ),
-      ),
-      body: Center(
-        // ignore: avoid_unnecessary_containers
-        child: Container(
-          child: const Text(
-            'Welcome to 30 days of  Flutter',
+    return SingleChildScrollView(
+      child: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 0, 44, 109),
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 0, 24, 59),
+          title: const Text(
+            'DevAcademy',
           ),
         ),
+        body: Center(
+          // ignore: avoid_unnecessary_containers
+          child: Container(
+            child: const Text(
+              'Welcome to 30 days of  Flutter',
+            ),
+          ),
+        ),
+        drawer: Drawer(),
       ),
-      drawer: Drawer(),
     );
   }
 }
