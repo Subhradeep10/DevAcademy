9d7452625b1bd3142580fdcfb501c28d56998ed9
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-03-29 21:10:47 +0530

    Landing Page is almost done

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -15,6 +15,7 @@ class HomePage extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             const Image(
               image: AssetImage('assets/images/Proud_Coder_image.png'),
@@ -29,60 +30,289 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: double.infinity,
-              height: 500,
-              color: Color.fromARGB(255, 252, 252, 252),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    // ignore: prefer_const_literals_to_create_immutables
+            Wrap(
+              spacing: 16.0, // gap between adjacent chips
+              runSpacing: 8.0, // gap between lines
+              direction: Axis.horizontal,
+
+              children: [
+                const Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Image(
+                      image: AssetImage('assets/images/Campus_Ambassador.png'),
+                      height: 150,
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Column(
                     children: [
+                      const Text(
+                        'Campus Ambassador',
+                        style: TextStyle(
+                            fontSize: 24,
+                            color: Color.fromARGB(255, 0, 19, 71),
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
                       const Padding(
-                        padding: EdgeInsets.all(20.0),
-                        child: Image(
-                          image:
-                              AssetImage('assets/images/Campus_Ambassador.png'),
-                          height: 200,
+                        padding:
+                            EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                        child: Text(
+                          "Place where you can find all the resources and details of ambassador/fellowship that are available across different countries and companies.",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color.fromARGB(255, 0, 68, 255),
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
                         ),
                       ),
-                      Center(
-                        child: Column(
-                          children: [
-                            const Text(
-                              'Campus Ambassador',
-                              style: TextStyle(
-                                fontSize: 24,
-                                color: Color.fromARGB(255, 0, 19, 71),
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 50,
-                              child: Text(
-                                "Place where you can find all the resources and details of ambassador/fellowship that are available across different countries and companies.",
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: Color.fromARGB(255, 0, 68, 255),
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                            ElevatedButton(
-                                onPressed: () {},
-                                child: const Text(
-                                  "View More",
-                                ))
-                          ],
-                        ),
+                      const SizedBox(
+                        height: 10,
                       ),
+                      ElevatedButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "View More",
+                          ))
                     ],
                   ),
-                ],
-              ),
+                ),
+                const Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Image(
+                      image: AssetImage('assets/images/Open_Source.png'),
+                      height: 150,
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Open Source',
+                        style: TextStyle(
+                            fontSize: 24,
+                            color: Color.fromARGB(255, 0, 19, 71),
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                        child: Text(
+                          "Here you can find all the resources and details of Open Source Programs and event that are available across different countries.",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color.fromARGB(255, 0, 68, 255),
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      ElevatedButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "View More",
+                          ))
+                    ],
+                  ),
+                ),
+                const Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Image(
+                      image: AssetImage('assets/images/Open_Source.png'),
+                      height: 150,
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Open Source',
+                        style: TextStyle(
+                            fontSize: 24,
+                            color: Color.fromARGB(255, 0, 19, 71),
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                        child: Text(
+                          "Here you can find all the resources and details of Open Source Programs and event that are available across different countries.",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color.fromARGB(255, 0, 68, 255),
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      ElevatedButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "View More",
+                          ))
+                    ],
+                  ),
+                ),
+                const Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Image(
+                      image: AssetImage('assets/images/Open_Source.png'),
+                      height: 150,
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Open Source',
+                        style: TextStyle(
+                            fontSize: 24,
+                            color: Color.fromARGB(255, 0, 19, 71),
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                        child: Text(
+                          "Here you can find all the resources and details of Open Source Programs and event that are available across different countries.",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color.fromARGB(255, 0, 68, 255),
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      ElevatedButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "View More",
+                          ))
+                    ],
+                  ),
+                ),
+                const Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Image(
+                      image: AssetImage('assets/images/Open_Source.png'),
+                      height: 150,
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Open Source',
+                        style: TextStyle(
+                            fontSize: 24,
+                            color: Color.fromARGB(255, 0, 19, 71),
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                        child: Text(
+                          "Here you can find all the resources and details of Open Source Programs and event that are available across different countries.",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color.fromARGB(255, 0, 68, 255),
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      ElevatedButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "View More",
+                          ))
+                    ],
+                  ),
+                ),
+                const Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Image(
+                      image: AssetImage('assets/images/Open_Source.png'),
+                      height: 150,
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Open Source',
+                        style: TextStyle(
+                            fontSize: 24,
+                            color: Color.fromARGB(255, 0, 19, 71),
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                        child: Text(
+                          "Here you can find all the resources and details of Open Source Programs and event that are available across different countries.",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color.fromARGB(255, 0, 68, 255),
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      ElevatedButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "View More",
+                          ))
+                    ],
+                  ),
+                ),
+              ],
             ),
           ],
         ),
