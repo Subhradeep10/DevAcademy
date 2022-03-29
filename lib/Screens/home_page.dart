294b380c7568b00055e19f7f8de4bc4c38a34030
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-03-29 20:03:18 +0530

    Images are loaded

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -36,23 +36,50 @@ class HomePage extends StatelessWidget {
               width: double.infinity,
               height: 500,
               color: Color.fromARGB(255, 252, 252, 252),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                // ignore: prefer_const_literals_to_create_immutables
+              child: Column(
                 children: [
-                  const Padding(
-                    padding: EdgeInsets.all(20.0),
-                    child: Image(
-                      image: AssetImage('assets/images/Campus_Ambassador.png'),
-                      height: 200,
-                    ),
-                  ),
-                  const Text(
-                    'Welcome To Developer Academy',
-                    style: TextStyle(
-                      fontSize: 24,
-                      color: Color.fromARGB(255, 0, 19, 71),
-                    ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: Image(
+                          image:
+                              AssetImage('assets/images/Campus_Ambassador.png'),
+                          height: 200,
+                        ),
+                      ),
+                      Center(
+                        child: Column(
+                          children: [
+                            const Text(
+                              'Campus Ambassador',
+                              style: TextStyle(
+                                fontSize: 24,
+                                color: Color.fromARGB(255, 0, 19, 71),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 50,
+                              child: Text(
+                                "Place where you can find all the resources and details of ambassador/fellowship that are available across different countries and companies.",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Color.fromARGB(255, 0, 68, 255),
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            ElevatedButton(
+                                onPressed: () {},
+                                child: const Text(
+                                  "View More",
+                                ))
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
