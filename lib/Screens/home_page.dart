403e450419a33ec95a41692bf57b2c1f5c196373
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-03-29 13:04:29 +0530

    Landing page is started

---
+++ lib/Screens/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 0, 44, 109),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 0, 24, 59),
+        title: const Text(
+          'DevAcademy',
+        ),
+      ),
+      body: Center(
+        // ignore: avoid_unnecessary_containers
+        child: Container(
+          child: const Text(
+            'Welcome to 30 days of  Flutter',
+          ),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dev_academy/Screens/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,8 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // ignore: avoid_unnecessary_containers
-    return Container(
-      child: const Text('Hello World'),
+    return MaterialApp(
+      routes: {
+        '/': (context) => HomePage(),
+      },
     );
   }
 }
