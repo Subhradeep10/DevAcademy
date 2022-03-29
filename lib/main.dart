1e9f39e6bbbd6e451d1274b2a165afd3007fdd0f
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-03-29 12:44:53 +0530

    Branch Deleted

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: avoid_unnecessary_containers
+    return Container(
+      child: const Text('Hello World'),
+    );
+  }
+}
