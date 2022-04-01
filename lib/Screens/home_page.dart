76c8f33e48c13f080ebee87f125b141c5c753a34
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-04-01 20:35:50 +0530

    Component is added

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:dev_academy/Landing_page_Component/Campus_Ambassador_component.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -39,56 +40,7 @@ class HomePage extends StatelessWidget {
                 direction: Axis.horizontal,
 
                 children: [
-                  const Center(
-                    child: Padding(
-                      padding: EdgeInsets.all(20.0),
-                      child: Image(
-                        image:
-                            AssetImage('assets/images/Campus_Ambassador.png'),
-                        height: 150,
-                      ),
-                    ),
-                  ),
-                  Center(
-                    child: Column(
-                      children: [
-                        const Text(
-                          'Campus Ambassador',
-                          style: TextStyle(
-                              fontSize: 24,
-                              color: Color.fromARGB(255, 0, 19, 71),
-                              fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        const Padding(
-                          padding:
-                              EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-                          child: Text(
-                            "Place where you can find all the resources and details of ambassador/fellowship that are available across different countries and companies.",
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Color.fromARGB(255, 0, 68, 255),
-                              fontWeight: FontWeight.bold,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20))),
-                            onPressed: () {},
-                            child: const Text(
-                              "View More",
-                            ))
-                      ],
-                    ),
-                  ),
+                  Campus_Ambassador_Component(),
                   const Center(
                     child: Padding(
                       padding: EdgeInsets.all(20.0),
