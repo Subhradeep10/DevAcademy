ce6d0c036237db5e9383ded03c38a40ea5d5aaee
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-04-01 21:47:25 +0530

    Code of conduct added

---
+++ lib/Screens/AppDevelopment.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppDevelopment extends StatelessWidget {
+  const AppDevelopment({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('App Development'),
+    );
+  }
+}
