817240bb29dcaefbd11ec952f03b6aa6f5348904
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-04-01 21:00:08 +0530

    Routes are added

---
+++ lib/Utils/Routes.dart
@@ -0,0 +1,7 @@
+class MyRoutes {
+  static String CampusAmbassador = '/CampusAmbassador';
+  static String OpenSource = '/OpenSource';
+  static String WebDevelopment = '/WebDevelopment';
+  static String AppDevelopment = '/AppDevelopment';
+  static String Toolkits = '/Toolkits';
+}
