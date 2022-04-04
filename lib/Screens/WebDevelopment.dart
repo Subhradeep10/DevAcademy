dd6e835ff0938b671b1e5216311694b2be8620ea
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-04-04 19:55:02 +0530

    Web Development data is Adding

---
+++ lib/Screens/WebDevelopment.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WebDevelopment extends StatelessWidget {
+  const WebDevelopment({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [Center(child: Card())],
+    );
+  }
+}
