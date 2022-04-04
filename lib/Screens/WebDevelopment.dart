8adc943b868c3f31e56ddbf3eb234f0e3ee13ea5
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-04-04 19:58:59 +0530

    Web Development data is Adding

--- lib/Screens/WebDevelopment.dart
+++ lib/Screens/WebDevelopment.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class WebDevelopment extends StatelessWidget {
+class WebDevelopment extends StatefulWidget {
   const WebDevelopment({Key? key}) : super(key: key);
 
+  @override
+  State<WebDevelopment> createState() => _WebDevelopmentState();
+}
+
+class _WebDevelopmentState extends State<WebDevelopment> {
   @override
   Widget build(BuildContext context) {
     return Column(
