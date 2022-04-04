1cd5f1febcad0807d5915a0f203656c8a85d4e18
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-04-04 19:42:44 +0530

    App Development is Adding

--- lib/Screens/AppDevelopment.dart
+++ lib/Screens/AppDevelopment.dart
@@ -5,8 +5,8 @@ class AppDevelopment extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('App Development'),
+    return Column(
+      children: const [Center(child: Card())],
     );
   }
 }
