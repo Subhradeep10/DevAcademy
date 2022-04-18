0e15894f456c2141849b5f5f1bf0fef9afb5aa89
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-04-18 12:04:54 +0530

    Add mob added

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,17 @@ import 'package:dev_academy/Screens/Toolkits.dart';
 import 'package:dev_academy/Screens/home_page.dart';
 import 'package:dev_academy/Utils/Routes.dart';
 import 'package:flutter/material.dart';
-
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'Screens/AppDevelopment.dart';
 import 'Screens/OpenSource.dart';
 import 'Screens/WebDevelopment.dart';
 import 'Screens/campus_ambassadar.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  final initFuture = MobileAds.instance.initialize(
+    appId: 'ca-app-pub-3940256099942544~3347511713',
+  );
   runApp(MyApp());
 }
 
