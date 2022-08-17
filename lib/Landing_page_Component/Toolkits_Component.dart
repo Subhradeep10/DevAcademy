3cd722862dbbadc92447d2fece02d4fc40cc5c04
nigeldias27 <nigeldias27@yahoo.com>
2022-08-17 23:30:37 +0530

    Added Inkwell widget to every component and removed the view more button. Added Scale Transition between the pages and changed how the text is assembled in each sub heading

--- lib/Landing_page_Component/Toolkits_Component.dart
+++ lib/Landing_page_Component/Toolkits_Component.dart
@@ -7,70 +7,68 @@ class Toolkits_Component extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        width: MediaQuery.of(context).size.width - 20,
-        decoration: const BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-            gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Color.fromARGB(255, 175, 21, 202),
-                  Color.fromARGB(255, 241, 152, 26)
-                ])),
-        child: Column(
-          children: [
-            const Center(
-              child: Padding(
-                padding: EdgeInsets.all(20.0),
-                child: Image(
-                  image: AssetImage('assets/images/Toolkits.png'),
-                  height: 150,
-                ),
-              ),
-            ),
-            Center(
-              child: Column(
-                children: [
-                  const Text(
-                    'Toolkits',
-                    style: TextStyle(
-                        fontSize: 24,
-                        color: Color.fromARGB(255, 253, 253, 255),
-                        fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-                    child: Text(
-                      "While the number of ways to organize Software is almost infinite, here are some collection of framework and libraries that can help you while you are working on a project or searching around the web or app",
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Color.fromARGB(255, 255, 255, 255),
-                        fontWeight: FontWeight.bold,
-                      ),
-                      textAlign: TextAlign.center,
+      child: InkWell(onTap: (){
+        Navigator.pushNamed(context, MyRoutes.Toolkits);
+      },splashColor: const Color(0xff673ab7),
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+        child: Ink(
+          child: Container(
+            width: MediaQuery.of(context).size.width - 20,
+            decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Color.fromARGB(255, 175, 21, 202),
+                      Color.fromARGB(255, 241, 152, 26)
+                    ])),
+            child: Column(
+              children: [
+                const Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Image(
+                      image: AssetImage('assets/images/Toolkits.png'),
+                      height: 150,
                     ),
                   ),
-                  const SizedBox(
-                    height: 10,
+                ),
+                Center(
+                  child: Column(
+                    children: const [
+                      Text(
+                        'Toolkits',
+                        style: TextStyle(
+                            fontSize: 24,
+                            color: Color.fromARGB(255, 253, 253, 255),
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                        child: Text(
+                          "While the number of ways to organize Software is almost infinite, here are some collection of framework and libraries that can help you while you are working on a project or searching around the web or app",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+
+                    ],
                   ),
-                  ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20))),
-                      onPressed: () {
-                        Navigator.pushNamed(context, MyRoutes.Toolkits);
-                      },
-                      child: const Text(
-                        "View More",
-                      ))
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
